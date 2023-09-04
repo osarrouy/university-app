c05f0086d2d7d71a59c0c5ec38972052edc8bde0
Olivier Sarrouy <me@osarrouy.bzh>
2023-09-04 16:05:40 +0200

    Try env variables again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		'process.env': process.env,
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
