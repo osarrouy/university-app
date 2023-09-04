0fd5712ec3703aa8628e330ff8960d45dfa58de5
Olivier Sarrouy <me@osarrouy.bzh>
2023-09-04 12:38:15 +0200

    Move to svelte-wagmi

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	const click = (ev) => {
+		dispatch('click', ev);
+	};
+</script>
+
+<button on:click={click}><slot /></button>
+
+<style lang="scss">
+	button {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		background-color: $light;
+		border: 1px solid $dark;
+		border-radius: $radius;
+		font-size: 0.8rem;
+		color: $dark;
+		cursor: pointer;
+		font-family: 'Supply Mono', monospace;
+		padding: $space-sm;
+		transition: all 0.3s;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+
+		&:focus {
+			color: #171e29;
+		}
+
+		&:hover {
+			border-color: $accent;
+			color: $accent;
+			fill: $accent;
+		}
+
+		&:active {
+			border-color: $accent;
+			color: $accent;
+			fill: $accent;
+		}
+	}
+
+	@media (min-width: 768px) {
+		button {
+			min-width: 170px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,82 @@
 <script>
-	import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
-	import { Web3Modal } from '@web3modal/html';
-	import { configureChains, createConfig } from '@wagmi/core';
-	import { arbitrum, mainnet, polygon } from '@wagmi/core/chains';
+	import {
+		connected,
+		web3Modal,
+		loading,
+		signerAddress,
+		wagmiLoaded,
+		chainId,
+		defaultConfig
+	} from 'svelte-wagmi';
+	import { mainnet, goerli } from '@wagmi/chains';
 	import { onMount } from 'svelte';
 
-	onMount(() => {
-		const chains = [arbitrum, mainnet, polygon];
-		const projectId = '1286a6606e040f0620d01ee2465cb56a';
+	import { Button } from '$lib/components';
 
-		const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-		const wagmiConfig = createConfig({
-			autoConnect: true,
-			connectors: w3mConnectors({ projectId, chains }),
-			publicClient
+	const PUBLIC_WALLETCONNECT_ID = '1286a6606e040f0620d01ee2465cb56a';
+
+	onMount(async () => {
+		const erckit = defaultConfig({
+			appName: 'erc.kit',
+			chains: [mainnet, goerli],
+			walletConnectProjectId: PUBLIC_WALLETCONNECT_ID
 		});
-		const ethereumClient = new EthereumClient(wagmiConfig, chains);
-		const web3modal = new Web3Modal({ projectId }, ethereumClient);
+
+		await erckit.init();
 	});
+
+	// const connect = async () => {
+	// 	const provider = await web3Modal.connect();
+	// 	console.log(provider);
+	// };
+
+	const connect = () => {
+		$web3Modal.openModal();
+	};
 </script>
 
+<header>
+	<h1>Rennes2 Dapp</h1>
+
+	<Button on:click={() => connect()}>
+		{#if $connected}
+			{$signerAddress}
+		{:else}
+			connect your wallet
+		{/if}
+	</Button>
+</header>
 <main>
-	<w3m-core-button />
+	{#if $connected}
+		<Button on:click={() => connect()}>claim ETH</Button>
+		<p>or</p>
+		<Button on:click={() => connect()}>go to dashboard</Button>
+	{/if}
 </main>
 
 <style lang="scss">
+	header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding: $space-md;
+		h1 {
+			font-size: 1.6rem;
+			font-weight: 600;
+		}
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: $space-md;
+		padding: $space-md;
+
+		p {
+			margin: $space-md 0;
+		}
+	}
 </style>
