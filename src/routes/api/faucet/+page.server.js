0cb25e16889629f1a28df8f7596ee74e4e2bde0e
Olivier Sarrouy <me@osarrouy.bzh>
2023-09-04 21:36:16 +0200

    Update mnemonic

--- src/routes/api/faucet/+page.server.js
+++ src/routes/api/faucet/+page.server.js
@@ -12,24 +12,19 @@ export const load = async (request, response) => {
 	const address = request.url.searchParams.get('address')
 	const amountInEther = '0.01';
 
-	let wallet = ethers.Wallet.fromPhrase('radio shy wolf unlock peanut shock olive entry cry honey page visa'); // 0x8cC0743af4C72866501F591D892e34c4167C8d19
-	console.log('1')
+	let wallet = ethers.Wallet.fromPhrase(process.env.MNEMONIC); // 0x6affb932e75147b64ccf3e0bdc1e5756f4fe968e
 	
 	const provider = new ethers.InfuraProvider('goerli', process.env.PROJECT_ID, process.env.PROJECT_SECRET);
 	console.log(provider.projectId)
 	console.log(provider.projectSecret)
-	
+
 
 	const tx = {
 		to: address,
 		value: ethers.parseEther(amountInEther)
 	};
-	console.log('2')
 	wallet = wallet.connect(provider);
-	console.log('3')
-
 	const receipt = await wallet.sendTransaction(tx);
-	console.log('4')
 
 	console.log(receipt.hash + ' => ' + address);
 	console.log('Balance : ' + (await wallet.getBalance()).toString());
