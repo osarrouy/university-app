a4b6dc464e7c75b874327f32cc6d151fd3cfa35b
Olivier Sarrouy <me@osarrouy.bzh>
2023-09-05 13:13:54 +0200

    Try edge functions

--- src/routes/api/faucet/+page.server.js
+++ src/routes/api/faucet/+page.server.js
@@ -1,30 +1,24 @@
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
-  runtime: 'nodejs18.x'
+  // runtime: 'nodejs18.x'
+	runtime: 'edge'
 };
 
 import { ethers } from 'ethers';
 
 const AMOUNT_ETH = '0.01';
  
-export const load = (request, response) => {
+export const load = async (request, response) => {
 	const address = request.url.searchParams.get('address')
 	const provider = new ethers.InfuraProvider('goerli', process.env.PROJECT_ID, process.env.PROJECT_SECRET);
 	const wallet = ethers.Wallet.fromPhrase(process.env.MNEMONIC).connect(provider); // 0x6affb932e75147b64ccf3e0bdc1e5756f4fe968e
 
-	wallet.sendTransaction({
+	await wallet.sendTransaction({
 		to: address,
 		value: ethers.parseEther(AMOUNT_ETH)
 	});
 
 	console.log(AMOUNT_ETH + ' ETH => ' + address);
 
-	 response.status(200).json({
-    body: JSON.stringify({ status: 'OK' }),
-		headers: {
-			'access-control-allow-origin': '*'
-		}
-  });
-
- 
+	return response.status(200).json({ title: 'OK', headers: { 'access-control-allow-origin': '*'} });
 }
